7d6e13829127fb0a6249f82ec5cf7711337da322
Kyle Cogan <kyle.cogan1993@gmail.com>
2024-02-16 14:38:34 -0500

    completed signup

---
+++ src/components/SignUpForm.jsx
@@ -0,0 +1,49 @@
+import React from 'react'
+import {useState} from 'react'
+
+export default function SignUpForm() {
+    
+    const [username, setUsername] = useState("");
+    const [password, setPassword] = useState("");
+    const [error, setError] = useState(null);
+    
+    async function handleSubmit(event) {
+        event.preventDefault();
+        try{
+            const response = await fetch("https://fsa-jwt-practice.herokuapp.com/signup", {
+                method: "POST",
+                body: JSON.stringify({
+                    username: {username},
+                    password: {password}
+                })
+            });
+            const json = await response.json();
+            console.log(json);
+
+        } catch (error) {
+            setError(error.message);
+        }
+    }
+
+    return (
+        <>
+            <h2>Sign Up</h2>
+            {error && <p>{error}</p> }
+            <form onSubmit={handleSubmit}>
+                <label>Username: 
+                    <input
+                        value={username}
+                        onChange={event => setUsername(event.target.value)}
+                    />
+                </label>
+                <label>Password: 
+                    <input
+                        value={password}
+                        onChange={event => setPassword(event.target.value)}
+                    />
+                </label>
+                <button>Submit</button>
+            </form>
+        </>
+        );
+}
